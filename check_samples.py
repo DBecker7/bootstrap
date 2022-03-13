6c7e5b97fea588aaf9829fa2498af1080f725301
Devan Becker <d7becker@gmail.com>
2022-03-12 22:13:23 -0500

    Good starting values make error checking unnecessary.

--- check_samples.py
+++ check_samples.py
@@ -14,21 +14,13 @@ k_tracker = np.array([])
 for i in range(1000):
     df_number = np.random.randint(0, 1000, 1)
     df_filename = "samples/sample_" + str(i).zfill(4) + ".csv"
-    try:
-        df = pd.read_csv(df_filename)
-    except:
-        print("df does not exist")
-        next
+    df = pd.read_csv(df_filename)
 
     try:
         popt, pcov = curve_fit(logistic_growth, df.x.to_numpy(), df.y.to_numpy(), p0 = [0.3, 15])
     except:
         print("No minimum found")
         next
-
-    if popt[1] < 0:
-        print(i)
-        #os.remove("samples/sample_" + str(i).zfill(4) + ".csv")
 
     r_tracker = np.append(r_tracker, popt[0])
     k_tracker = np.append(k_tracker, popt[1])
@@ -44,8 +36,8 @@ axis[0, 0].set_title("r = " + str(np.round(popt[0], 3)) + " and k = " + str(np.r
 axis[0, 1].plot(r_tracker, k_tracker, "o")
 
 
-axis[1, 0].hist(r_tracker)
-axis[1, 1].hist(k_tracker)
+axis[1, 0].hist(r_tracker, bins = 50)
+axis[1, 1].hist(k_tracker, bins = 50)
 
 plt.show()
 
